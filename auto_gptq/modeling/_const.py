7e395b431a00b46e418e3969dd45be9e672e7625
zhangsicheng1024 <zhangsicheng1024@163.com>
2023-07-27 09:32:04 +0000

    init commit from AutoGPTQ v0.3.0

---
+++ auto_gptq/modeling/_const.py
@@ -0,0 +1,27 @@
+from packaging.version import parse as parse_version
+
+from torch import device
+
+from ..utils.import_utils import compare_transformers_version
+
+CPU = device("cpu")
+CUDA_0 = device("cuda:0")
+
+SUPPORTED_MODELS = [
+    "bloom",
+    "gptj",
+    "gpt2",
+    "gpt_neox",
+    "opt",
+    "moss",
+    "gpt_bigcode",
+    "codegen",
+    "RefinedWebModel",
+    "RefinedWeb",
+    "baichuan",
+    "internlm",
+]
+if compare_transformers_version("v4.28.0", op="ge"):
+    SUPPORTED_MODELS.append("llama")
+
+__all__ = ["CPU", "CUDA_0", "SUPPORTED_MODELS"]
---
+++ auto_gptq/utils/import_utils.py
@@ -0,0 +1,49 @@
+from packaging.version import parse as parse_version
+
+try:
+    import triton
+
+    TRITON_AVAILABLE = True
+except ImportError:
+    TRITON_AVAILABLE = False
+
+try:
+    import autogptq_cuda
+
+    AUTOGPTQ_CUDA_AVAILABLE = True
+except:
+    AUTOGPTQ_CUDA_AVAILABLE = False
+
+
+def dynamically_import_QuantLinear(use_triton: bool, desc_act: bool, group_size: int):
+    if use_triton:
+        from ..nn_modules.qlinear.qlinear_triton import QuantLinear
+    else:
+        if not desc_act or group_size == -1:
+            from ..nn_modules.qlinear.qlinear_cuda_old import QuantLinear
+        else:
+            from ..nn_modules.qlinear.qlinear_cuda import QuantLinear
+
+    return QuantLinear
+
+
+def compare_transformers_version(
+    version: str = "v4.28.0",
+    op: str = "eq"
+):
+    assert op in ["eq", "lt", "le", "gt", "ge"]
+
+    from transformers import __version__
+
+    return getattr(parse_version(__version__), f"__{op}__")(parse_version(version))
+
+
+def compare_pytorch_version(
+    version: str = "v2.0.0",
+    op: str = "eq"
+):
+    assert op in ["eq", "lt", "le", "gt", "ge"]
+
+    from torch import __version__
+
+    return getattr(parse_version(__version__), f"__{op}__")(parse_version(version))
